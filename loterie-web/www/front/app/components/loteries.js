c8c152801875364208bcbda7bd0af40845b927dc
Maxence <maxence.lagourgue@free.fr>
2025-12-11 11:47:14 +0100

    changing loterie to add link

--- loterie-web/www/front/app/components/loteries.js
+++ loterie-web/www/front/app/components/loteries.js
@@ -3,6 +3,7 @@
 import { useGetLoteries } from "@/app/contract/loterie"
 import { useQueryClient } from '@tanstack/react-query'
 import { useEffect } from "react"
+import Link from "next/link";
 
 export const Loteries = () => {
     const queryClient = useQueryClient()
@@ -19,7 +20,9 @@ export const Loteries = () => {
         <ul>
             {addresses.map((addr, i) => (
                 <li key={addr}>
-                    {names[i]} - {addr}
+                    <Link href={`/loterie/${addr}`}>
+                        {names[i]} - {addr}
+                    </Link>
                 </li>
             ))}
         </ul>
