532e106bbf33015bcb04a1dd0d5deea629bb728f
Maxence <maxence.lagourgue@free.fr>
2025-12-12 10:27:08 +0100

    Changes

--- loterie-web/www/front/app/wagmi/config.js
+++ loterie-web/www/front/app/wagmi/config.js
@@ -1,12 +1,12 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { http, WagmiProvider, createConfig } from 'wagmi'
-import { mainnet, linea, lineaSepolia, sepolia } from 'wagmi/chains'
+import { sepolia } from 'wagmi/chains'
 import { metaMask } from 'wagmi/connectors'
 
 
 export const config = createConfig({
     ssr: true, // Enable this if your dapp uses server-side rendering.
-    chains: [mainnet, linea, lineaSepolia, sepolia],
+    chains: [sepolia],
     connectors: [
         metaMask({
             infuraAPIKey: "713df48955dd494984810e5642680efa",
